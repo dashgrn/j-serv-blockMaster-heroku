94aa7385e89812e9ee21be02a8607b79eb0d7445
Jose Fernandez <josesk20@gmail.com>
2021-11-04 19:37:45 -0500

    dbname

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('dbProducts.json'); 
+const router = jsonServer.router('playlist.json'); 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; //
 
